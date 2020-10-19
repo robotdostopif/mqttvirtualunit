5ed9a0440939302607f8a03a26bb2998825d14a2
Sam <samuel.bjorkqvist@gmail.com>
2020-10-19 12:10:27 +0200

    Added VirtualUnit class

--- virtualunit.py
+++ virtualunit.py
@@ -1,6 +1,45 @@
+import random
 import paho.mqtt.publish as publish
-from app_settings import AppSettings
+# from app_settings import AppSettings
 
-config = AppSettings()
-publish.single(config.mqttbroker.mqttusername + "/feeds/virtualunit", "hayload", hostname="io.adafruit.com",
-               auth={'username': config.mqttbroker.mqttusername , 'password': config.mqttbroker.mqttpassword })
+# config = AppSettings()
+
+
+class VirtualUnit:
+    Id = ""
+    beaconId = ""
+    rssi = 0
+    beaconIds = []
+
+    def generateRssi(self):
+        rssi = random.randint(-80, -30)
+        print(rssi)
+
+    # def generateId():
+
+    # def generateBeaconId():
+
+    # def setInterval():
+
+    # publish.single(config.mqttbroker.mqttusername + "/feeds/virtualunit", "heyload", hostname="io.adafruit.com",
+    #                auth={'username': config.mqttbroker.mqttusername, 'password': config.mqttbroker.mqttpassword})
+
+#     VirtualUnit
+# const string Id;
+# int payloadId;
+# int rssi;
+# const List<string> payloadIds;
+
+# generaterssi()
+# generateid()
+# generatePayloadId()
+
+# setInterval(int seconds)
+# {
+# publishTimer(seconds)
+# timer.start()
+# while timer.value < seconds
+# generatePayloadId()
+# generaterssi()
+# client.Publish()
+# }
---
+++ virtualunitprogram.py
@@ -0,0 +1,4 @@
+from virtualunit import VirtualUnit
+
+vu = VirtualUnit()
+vu.generateRssi()
