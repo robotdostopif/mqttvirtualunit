57a638e8f1d60dc627f9a7a0f6b3dcf1c0861beb
robotdostopif <aron.cederlund@outlook.com>
2020-10-21 10:03:43 +0200

    Added referencedata implementation

--- server.py
+++ server.py
@@ -15,7 +15,13 @@ driver = config.sqlconnection.sqldriver
 def myconverter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
-
+def get_reference_data(beaconID):
+    file = open('config/assetreference.json')
+    data = json.load(file)
+    for d in data:
+        if d['beaconId'] == beaconID:
+            file.close()
+            return d
 @anvil.server.callable
 def get_event_data():
     data = []
@@ -24,7 +30,9 @@ def get_event_data():
         cur.execute('SELECT * FROM Events')
         rows = cur.fetchall()
         for row in rows:
-            data.append({'id': row[0], 'type': row[1], 'recieverId' :row[2], 'beaconId' :row[3], 'rssi':row[4]})
+            refData = get_reference_data(row[3])
+            print(refData)
+            data.append({'description':refData['description'],'id': row[0], 'type': row[1], 'recieverId' :row[2], 'beaconId' :row[3], 'rssi':row[4]})
         return data
 
 # Create a TCP/IP socket
@@ -35,7 +43,7 @@ server_address = (server_name, 10000)
 print('starting up on %s port %s' % server_address, file=sys.stderr)
 sock.bind(server_address)
 sock.listen(1)
-
+get_reference_data('5adb6608-1171-4d8c-a3fc-50e2e645ea81')
 while True:             # instead of anvil.server.run_forever()
     time.sleep(1)
     print('waiting for a connection', file=sys.stderr)
