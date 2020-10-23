d9e136cb04b6649e3671f33bfa3e177331c3397a
robodostopif <boomeraron@gmail.com>
2020-10-23 13:59:46 +0200

    Edited subscribe function

--- mqttsubscribe.py
+++ mqttsubscribe.py
@@ -1,35 +1,34 @@
-import paho.mqtt.subscribe as subscribe
 import datetime
 import pyodbc
 import json
+import paho.mqtt.client as paho
 from app_settings import AppSettings
 
 config = AppSettings()
-mqttusername = config.mqttbroker.mqttusername
-mqttpassword = config.mqttbroker.mqttpassword
-mqttfeedpath = config.mqttbroker.mqttfeedpath
 server = config.sqlconnection.sqlserver
 database = config.sqlconnection.sqldatabase
 driver = config.sqlconnection.sqldriver
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
-def subscribe_message():
-    messagesJson = subscribe.simple(mqttusername + "/feeds/" + mqttfeedpath, hostname="io.adafruit.com",
-                       auth={'username': mqttusername, 'password': mqttpassword})
-    messagesObj = json.loads(messagesJson.payload)
-    print(messagesObj)
+def on_message(client, userdata, msg):
+    add_message_to_database(msg)
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
+
+client = paho.Client()
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+username_pw_set(config.mqttbroker.mqttusername, password=config.mqttbroker.mqttpassword)
+client.connect(“io.adafruit.com”, 1883)
+client.subscribe(“config.mqttusername + "/feeds/" + config.mqttfeedpath, qos=1)
+
+client.loop_forever()
+
+    
+def add_message_to_database(messagesObj):
     dbCall = pyodbc.connect('Driver='+driver+'; SERVER='+server+'; DATABASE='+database+'; Trusted_Connection=True;')
     cursor = dbCall.cursor()
     sql = "INSERT INTO Events (RecieverId, BeaconId, Rssi, Created) VALUES (?, ?, ?, ?)"
     val = (messagesObj['RecieverId'], messagesObj['BeaconId'], messagesObj['Rssi'], datetime.datetime.now())
     cursor.execute(sql,val)
     dbCall.commit()
-
-def main():
-    try:
-        while True:
-            subscribe_message()
-            print("Press Ctrl + C to Exit to stop subscribe.")
-    except KeyboardInterrupt:
-        pass
-
-main()
