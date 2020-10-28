10d7071f9aefaabc8668c6a2c77df60bb372da61
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-10-28 11:11:02 +0100

    Removed the socket setup code and added wait_forever

--- server.py
+++ server.py
@@ -23,33 +23,4 @@ def get_event_data():
     return data
 
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server_name = 'localhost'  # user sys.argv[1] to bind the socket to the address given on the command line
-server_address = (server_name, 10000)
-print('starting up on %s port %s' % server_address, file=sys.stderr)
-sock.bind(server_address)
-sock.listen(1)
-while True:  # instead of anvil.server.run_forever()
-    time.sleep(1)
-    print('waiting for a connection', file=sys.stderr)
-    connection, client_address = sock.accept()
-    try:
-        print('client connected:', client_address, file=sys.stderr)
-        total_received_data = []
-        while True:
-            try:
-                data = connection.recv(16)
-                print('received "%s"' % data, file=sys.stderr)
-                if data:
-                    total_received_data += data
-                    connection.sendall(data)
-                else:
-                    break
-            except KeyboardInterrupt:
-                print("Stop me!")
-                sys.exit(0)
-
-    finally:
-        connection.close()
+anvil.server.wait_forever()
