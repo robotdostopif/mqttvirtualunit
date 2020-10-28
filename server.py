6f6b4423d06053b1ee478d80772c36846434ccb7
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-10-28 11:15:28 +0100

    further refactoring, removed unused imports and variables

--- server.py
+++ server.py
@@ -1,19 +1,10 @@
-import anvil
 import anvil.server
-import socket
-import sys
-import time
-import pyodbc
-import json
-import datetime
 from app_settings import AppSettings
 from database import DataBase
 
 config = AppSettings()
+
 anvil.server.connect(config.anvil.uplink)
-server = config.sqlconnection.sqlserver
-database = config.sqlconnection.sqldatabase
-driver = config.sqlconnection.sqldriver
 
 
 @anvil.server.callable
